1e344cc5882a2d3b13ab255ce549fa3f266e7dbb
Michael k <michaelkahnt123@gmail.com>
2018-05-13 17:17:16 +0200

    Add files via upload

---
+++ 6.2 variablen im speicher anzeigen.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	
+	int energie;
+	int leistung;
+	
+	cout << "die adresse der variablen \"energie\"  lautet: " << &energie  << endl;
+	cout << "die adresse der variablen \"leistung\" lautet: " << &leistung << endl;
+	
+	return 0;
+}
