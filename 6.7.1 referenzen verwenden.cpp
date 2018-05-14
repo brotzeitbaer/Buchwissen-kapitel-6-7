a80b5581cb9dddc8ad2956eb9ef2b48ad69c39ed
Michael k <michaelkahnt123@gmail.com>
2018-05-14 20:19:54 +0200

    Update 6.7.1 referenzen verwenden.cpp

--- 6.7.1 referenzen verwenden.cpp
+++ 6.7.1 referenzen verwenden.cpp
@@ -8,6 +8,10 @@ int main()
 	int &rarea1  = area;
 	int &rarea2  = area;
 	
+/*in zeile 8 und 9 werden refernzen erzeugt, das sind quasi
+spiegelbilder. referenzen werden "alias" genannt.
+man beachte das referenzen KEINE kopien sind.*/
+	
 	cout << "wert   area: (original   wert) " << area   <<   endl;
 	cout << "wert rarea1: (referenz wert 1) " << rarea1 <<   endl;
 	cout << "wert rarea2: (referenz wert 2) " << rarea2 << "\n\n";
@@ -29,6 +33,10 @@ rarea2 += 25;
 	cout << "wert    area: (original  wert) " << area   <<   endl;
 	cout << "wert  rarea1: (referenz wert1) " << rarea1 <<   endl;
 	cout << "wert  rarea2: (referenz wert2) " << rarea2 << "\n\n";
+	
+	cout << "adresse   area: (original  wert) " << &area    <<   endl;
+	cout << "adresse rarea1: (referenz wert1) " << &rarea1  <<   endl;
+	cout << "adresse rarea2: (referenz wert2) " << &rarea2  <<   endl;
 	
 	return 0;
 }
